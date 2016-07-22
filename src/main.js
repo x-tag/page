5033be8712d90a00ef460f3e8c25b81dec461a54
Daniel Buchner <danieljb2@gmail.com>
2016-07-22 06:49:22 -0700

    finally nailed it

--- src/main.js
+++ src/main.js
@@ -1,27 +1,32 @@
 (function(){
 
-  var selected;
-
   xtag.register('x-page', {
     accessors: {
       active: {
         attribute: { boolean: true },
+        set: function(val, old){
+          if (val !== old) xtag.transition(this, val ? 'active' : 'inactive');
+        }
       },
       selected: {
         attribute: {
           boolean: true
         },
         set: function(val, old){
-          if (val) selected = this;
-          if (val && val != old && this.parentNode) xtag.query(document, 'x-page').forEach(function(node){
-            if (node != this) node.selected = false;
-          }, this);
+          if (val && val != old && this.parentNode) {
+              xtag.query(document, 'x-page').forEach(function(node){
+              if (node != this) node.selected = false;
+            }, this);
+          }
         }
       }
     },
     methods: {
-      show: function(){
+      show: function(deactivate){
         this.selected = true;
+        if (deactivate) xtag.query(document, 'x-page').forEach(function(node){
+          node.active = false;
+        });
         this.active = true;
       },
       hide: function(){
